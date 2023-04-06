c14a75246654a3e1621e0fc1bf87647bd1709390
Valenciana22 <Antoniovalenciana22@gmail.com>
2023-04-06 14:38:53 +1200

    Added Character tags and Scripts
    I added a script to the soldier for movement and varaibles for damage and Health.
    
    I didn't add a script for the  for the tank since I need to implement how the control will shift from character to character.
    
    I am also trying to implement a proper damage and health system

---
+++ Desecration of The Drowned/Assets/DrownedSoldierScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrownedSoldierScript : MonoBehaviour
+{
+    [Header("Stats")]
+    public int health = 20;
+    public int damage = 5; 
+
+    [Header("Movement")]
+    public float speed = 1.0f; //This is not doing anything at the moment but will kick in when I add AI Movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Desecration of The Drowned/Assets/SoldierScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SoldierScript : MonoBehaviour
+{
+    [Header("Stats")]
+    public int health = 100;
+    public int damage = 1;  
+    /*what if the damage is dynamic and changes within a range, like 2-4 or 5-10 based of certain weapons
+    or pick ups that you obtain. You will need to randomize these values. But only while you are shooting!
+    Maybe like... on button press, release a projectile and on trigger randomize damage value. But damage needs
+    to come from the character, not the projectile itself
+    */
+
+    [Header("Movement")]
+    public float speed = 1.0f;
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World!");
+        rb2d = GetComponent<Rigidbody2D>(); //This is how rigidbody get instantiated and movement is allowed
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Transform>().position += new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0) * speed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        //====standard rigidbody2d movement method, you should probably use something like this!
+        rb2d.MovePosition(transform.position + (new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")) * Time.fixedDeltaTime * speed));
+        
+        //GetComponent<Rigidbody2D>().MovePosition(transform.position + (new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")) * Time.fixedDeltaTime * speed));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+
+       /* if(other.tag == "Sun"){
+            score += 1;
+            points.text = score.ToString();
+            transform.localScale *= 1.025f;
+            other.GetComponent<SunCollapse>().Consume();
+            
+        } */
+        if(other.tag == "Enemy"){
+            Debug.Log("You got hit!");
+            //Maybe you can grab a parameter from the enemy to see how much damage it deals instead of hardcoding it 
+            health -= 5; 
+
+            //SceneManager.LoadScene("MainMenu");
+        }
+
+        
+
+    }
+}
